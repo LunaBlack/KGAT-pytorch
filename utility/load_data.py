6e5973b3439f1b74e68a6f040aa1fb30f2562155
LunaBlack <luna041959@gmail.com>
2019-12-15 03:07:57 +0800

    load data

---
+++ utility/load_data.py
@@ -0,0 +1,260 @@
+import os
+import random
+import collections
+
+import dgl
+import torch
+import numpy as np
+import pandas as pd
+
+
+class DataLoader(object):
+
+    def __init__(self, args):
+        self.args = args
+        self.data_name = args.data_name
+        self.use_KG = args.use_KG
+        self.use_pretrain = args.use_pretrain
+
+        self.cf_batch_size = args.cf_batch_size
+        self.kg_batch_size = args.kg_batch_size
+
+        data_dir = args.data_dir
+        train_file = os.path.join(data_dir, 'train.txt')
+        test_file = os.path.join(data_dir, 'test.txt')
+        kg_file = os.path.join(data_dir, "kg_final.txt")
+
+        self.cf_train_data, self.train_user_dict = self.load_cf(train_file)
+        self.cf_test_data, self.test_user_dict = self.load_cf(test_file)
+        self.statistic_cf()
+
+        kg_data = self.load_kg(kg_file)
+        self.construct_data(kg_data)
+
+        self.print_info()
+        self.train_graph = self.create_graph(self.kg_train_data, self.n_users_entities)
+        self.test_graph = self.create_graph(self.kg_test_data, self.n_users_entities)
+
+
+    def load_cf(self, filename):
+        user = []
+        item = []
+        user_dict = dict()
+
+        lines = open(filename, 'r').readlines()
+        for l in lines:
+            tmp = l.strip()
+            inter = [int(i) for i in tmp.split()]
+
+            if len(inter) > 1:
+                user_id, item_ids = inter[0], inter[1:]
+                item_ids = list(set(item_ids))
+
+                for item_id in item_ids:
+                    user.append(user_id)
+                    item.append(item_id)
+                user_dict[user_id] = item_ids
+
+        user = np.array(user, dtype=np.int32)
+        item = np.array(item, dtype=np.int32)
+        return (user, item), user_dict
+
+
+    def statistic_cf(self):
+        self.n_users = max(max(self.cf_train_data[0]), max(self.cf_test_data[0])) + 1
+        self.n_items = max(max(self.cf_train_data[1]), max(self.cf_test_data[1])) + 1
+        self.n_cf_train = len(self.cf_train_data)
+        self.n_cf_test = len(self.cf_test_data)
+
+
+    def load_kg(self, filename):
+        kg_data = pd.read_csv(filename, sep=' ', names=['h', 'r', 't'], engine='python')
+        kg_data = kg_data.drop_duplicates()
+        return kg_data
+
+
+    def construct_data(self, kg_data):
+        # plus inverse kg data
+        n_relations = max(kg_data['r']) + 1
+        reverse_kg_data = kg_data.copy()
+        reverse_kg_data = reverse_kg_data.rename({'h': 't', 't': 'h'}, axis='columns')
+        reverse_kg_data['r'] += n_relations
+        kg_data = pd.concat([kg_data, reverse_kg_data], ignore_index=True)
+
+        # re-map user id
+        kg_data['r'] += 2
+        self.n_relations = max(kg_data['r']) + 1
+        self.n_entities = max(max(kg_data['h']), max(kg_data['t'])) + 1
+        self.n_users_entities = self.n_users + self.n_entities
+
+        self.cf_train_data = (np.array(list(map(lambda d: d + self.n_entities, self.cf_train_data[0]))).astype(np.int32), self.cf_train_data[1].astype(np.int32))
+        self.cf_test_data = (np.array(list(map(lambda d: d + self.n_entities, self.cf_test_data[0]))).astype(np.int32), self.cf_test_data[1].astype(np.int32))
+
+        self.train_user_dict = {k + self.n_entities: np.unique(v).astype(np.int32) for k, v in self.train_user_dict.items()}
+        self.test_user_dict = {k + self.n_entities: np.unique(v).astype(np.int32) for k, v in self.test_user_dict.items()}
+
+        # add interactions to kg data
+        cf2kg_train_data = pd.DataFrame(np.zeros((self.n_cf_train, 3), dtype=np.int32), columns=['h', 'r', 't'])
+        cf2kg_train_data['h'] = self.cf_train_data[0]
+        cf2kg_train_data['t'] = self.cf_train_data[1]
+
+        reverse_cf2kg_train_data = pd.DataFrame(np.ones((self.n_cf_train, 3), dtype=np.int32), columns=['h', 'r', 't'])
+        reverse_cf2kg_train_data['h'] = self.cf_train_data[1]
+        reverse_cf2kg_train_data['t'] = self.cf_train_data[0]
+
+        cf2kg_test_data = pd.DataFrame(np.zeros((self.n_cf_test, 3), dtype=np.int32), columns=['h', 'r', 't'])
+        cf2kg_test_data['h'] = self.cf_test_data[0]
+        cf2kg_test_data['t'] = self.cf_test_data[1]
+
+        reverse_cf2kg_test_data = pd.DataFrame(np.ones((self.n_cf_test, 3), dtype=np.int32), columns=['h', 'r', 't'])
+        reverse_cf2kg_test_data['h'] = self.cf_test_data[1]
+        reverse_cf2kg_test_data['t'] = self.cf_test_data[0]
+
+        self.kg_train_data = pd.concat([kg_data, cf2kg_train_data, reverse_cf2kg_train_data], ignore_index=True)
+        self.kg_test_data = pd.concat([kg_data, cf2kg_test_data, reverse_cf2kg_test_data], ignore_index=True)
+
+        self.n_kg_train = len(self.kg_train_data)
+        self.n_kg_test = len(self.kg_test_data)
+
+        # construct kg dict
+        self.train_kg_dict = collections.defaultdict(list)
+        self.train_relation_dict = collections.defaultdict(list)
+        for row in self.kg_train_data.iterrows():
+            h, r, t = row[1]
+            self.train_kg_dict[h].append((t, r))
+            self.train_relation_dict[r].append((h, t))
+
+        self.test_kg_dict = collections.defaultdict(list)
+        self.test_relation_dict = collections.defaultdict(list)
+        for row in self.kg_test_data.iterrows():
+            h, r, t = row[1]
+            self.test_kg_dict[h].append((t, r))
+            self.test_relation_dict[r].append((h, t))
+
+
+    def print_info(self):
+        print('n_users:             ', self.n_users)
+        print('n_items:             ', self.n_items)
+        print('n_entities:          ', self.n_entities)
+        print('n_users_entities:    ', self.n_users_entities)
+        print('n_relations:         ', self.n_relations)
+
+        print('n_cf_train:          ', self.n_cf_train)
+        print('n_cf_test:           ', self.n_cf_test)
+
+        print('n_kg_train:          ', self.n_kg_train)
+        print('n_kg_test:           ', self.n_kg_test)
+
+
+    def create_graph(self, kg_data, n_nodes):
+        g = dgl.DGLGraph()
+        g.add_nodes(n_nodes)
+        g.add_edges(kg_data['t'], kg_data['h'])
+        g.readonly()
+        g.ndata['id'] = torch.arange(n_nodes, dtype=torch.long)
+        g.edata['type'] = torch.LongTensor(kg_data['r'])
+        return g
+
+
+    def sample_pos_items_for_u(self, user_dict, user_id, n_sample_pos_items):
+        pos_items = user_dict[user_id]
+        n_pos_items = len(pos_items)
+
+        sample_pos_items = []
+        while True:
+            if len(sample_pos_items) == n_sample_pos_items:
+                break
+
+            pos_item_idx = np.random.randint(low=0, high=n_pos_items, size=1)[0]
+            pos_item_id = pos_items[pos_item_idx]
+            if pos_item_id not in sample_pos_items:
+                sample_pos_items.append(pos_item_id)
+        return sample_pos_items
+
+
+    def sample_neg_items_for_u(self, user_dict, user_id, n_sample_neg_items):
+        pos_items = user_dict[user_id]
+
+        sample_neg_items = []
+        while True:
+            if len(sample_neg_items) == n_sample_neg_items:
+                break
+
+            neg_item_id = np.random.randint(low=0, high=self.n_items, size=1)[0]
+            if neg_item_id not in pos_items and neg_item_id not in sample_neg_items:
+                sample_neg_items.append(neg_item_id)
+        return sample_neg_items
+
+
+    def generate_cf_batch(self, user_dict):
+        exist_users = user_dict.keys()
+        if self.cf_batch_size <= len(exist_users):
+            batch_user = random.sample(exist_users, self.cf_batch_size)
+        else:
+            batch_user = [random.choice(exist_users) for _ in range(self.cf_batch_size)]
+
+        batch_pos_item, batch_neg_item = [], []
+        for u in batch_user:
+            batch_pos_item += self.sample_pos_items_for_u(user_dict, u, 1)
+            batch_neg_item += self.sample_neg_items_for_u(user_dict, u, 1)
+        return batch_user, batch_pos_item, batch_neg_item
+
+
+    def sample_pos_triples_for_h(self, kg_dict, head, n_sample_pos_triples):
+        pos_triples = kg_dict[head]
+        n_pos_triples = len(pos_triples)
+
+        sample_relations, sample_pos_tails = [], []
+        while True:
+            if len(sample_relations) == n_sample_pos_triples:
+                break
+
+            pos_triple_idx = np.random.randint(low=0, high=n_pos_triples, size=1)[0]
+            tail = pos_triples[pos_triple_idx][0]
+            relation = pos_triples[pos_triple_idx][1]
+
+            if relation not in sample_relations and tail not in sample_pos_tails:
+                sample_relations.append(relation)
+                sample_pos_tails.append(tail)
+        return sample_relations, sample_pos_tails
+
+
+    def sample_neg_triples_for_h(self, kg_dict, head, relation, n_sample_neg_triples):
+        pos_triples = kg_dict[head]
+
+        sample_neg_tails = []
+        while True:
+            if len(sample_neg_tails) == n_sample_neg_triples:
+                break
+
+            tail = np.random.randint(low=0, high=self.n_users_entities, size=1)[0]
+            if (tail, relation) not in pos_triples and tail not in sample_neg_tails:
+                sample_neg_tails.append(tail)
+        return sample_neg_tails
+
+
+    def generate_kg_batch(self, kg_dict):
+        exist_heads = kg_dict.keys()
+        if self.kg_batch_size <= len(exist_heads):
+            batch_head = random.sample(exist_heads, self.kg_batch_size)
+        else:
+            batch_head = [random.choice(exist_heads) for _ in range(self.kg_batch_size)]
+
+        batch_relation, batch_pos_tail, batch_neg_tail = [], [], []
+        for h in batch_head:
+            relation, pos_tail = self.sample_pos_triples_for_h(kg_dict, h, 1)
+            batch_relation += relation
+            batch_pos_tail += pos_tail
+
+            neg_tail = self.sample_neg_triples_for_h(kg_dict, h, relation[0], 1)
+            batch_neg_tail += neg_tail
+        return batch_head, batch_relation, batch_pos_tail, batch_neg_tail
+
+
+
+
+
+
+
+
+
